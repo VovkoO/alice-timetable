19d29639e8ef0a898f667dde179581ef6397c2eb
VovkoO <razdrogin.v@yandex.ru>
2019-02-21 21:59:30 +0300

    static files

--- alice/admin.py
+++ alice/admin.py
@@ -7,23 +7,23 @@ from .models import Univercity, Group, Lesson, Dates
 #     list_display = ('name')
 
 
-# class GroupAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'univercity_id')
-#
-#
-# class LessonAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'teacher', 'classroom', 'start_time', 'end_time', 'group_id')
-#
-#
-# class DatesAdmin(admin.ModelAdmin):
-#     list_display = ('date', 'lesson_id')
-#
-#
-# # admin.site.register(Univercity, UnivercityAdmin)
-# admin.site.register(Group, GroupAdmin)
-# admin.site.register(Lesson, LessonAdmin)
-# admin.site.register(Dates, DatesAdmin)
-#
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'univercity_id')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'teacher', 'classroom', 'start_time', 'end_time', 'group_id')
+
+
+class DatesAdmin(admin.ModelAdmin):
+    list_display = ('date', 'lesson_id')
+
+
+# admin.site.register(Univercity, UnivercityAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Dates, DatesAdmin)
+
 #
 
 
