fbe8e7280fc833a8277b06a66d97d32955d80873
VovkoO <razdrogin.v@yandex.ru>
2019-02-21 19:40:51 +0300

    First db

--- alice/admin.py
+++ alice/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Univercity, Group, Lesson, Dates
 # Register your models here.
+
+
+# class UnivercityAdmin(admin.ModelAdmin):
+#     list_display = ('name')
+
+
+# class GroupAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'univercity_id')
+#
+#
+# class LessonAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'teacher', 'classroom', 'start_time', 'end_time', 'group_id')
+#
+#
+# class DatesAdmin(admin.ModelAdmin):
+#     list_display = ('date', 'lesson_id')
+#
+#
+# # admin.site.register(Univercity, UnivercityAdmin)
+# admin.site.register(Group, GroupAdmin)
+# admin.site.register(Lesson, LessonAdmin)
+# admin.site.register(Dates, DatesAdmin)
+#
+#
+
+
--- alice/models.py
+++ alice/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Univercity(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=20)
+    univercity_id = models.ForeignKey(Univercity, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.CharField(max_length=20)
+    classroom = models.CharField(max_length=10)
+    start_time = models.TimeField(auto_now=False, null=True)
+    end_time = models.TimeField(auto_now=False, null=True)
+    group_id = models.ForeignKey(Univercity, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Dates(models.Model):
+    date = models.DateField(auto_now=False)
+    lesson_id = models.ForeignKey(Univercity, on_delete=models.CASCADE, null=True, blank=True)
